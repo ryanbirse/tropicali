f50d23128e37c2f4b4f1834e8aea80ed548f83b7
Ryan Birse <ryan@studiocollate.co.uk>
2018-10-24 20:16:43 +0100

    github deploy script added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var browserSync = require('browser-sync').create()
 
 var imagemin = require('gulp-imagemin')
 
+var ghpages = require('gh-pages')
+
 sass.compiler = require('node-sass')
 
 gulp.task("sass", function() {
@@ -52,6 +54,10 @@ gulp.task("watch", function() {
   gulp.watch("src/css/app.scss", ["sass"])
   gulp.watch("src/fonts/*", ["fonts"])
   gulp.watch("src/img*", ["images"])
+})
+
+gulp.task("deploy", function (){
+  ghpages.publish("dist")
 })
 
 gulp.task('default', ["html", "sass", "fonts", "images", "watch"])
